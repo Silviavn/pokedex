a3f306871c814bdec6931d1997aaf3ef30c0c11e
silvia <silviavergara.14sa@gmail.com>
2022-11-25 15:50:27 -0300

    pokemon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+
+
+
+
+
     const [pokemon, setPokemon] = useState('')
     const [pokemonD, setPokemonD] = useState({ sprites: { front_default: null } })
 
@@ -22,15 +27,15 @@ function App() {
     }
     return (
         <>
-            <div className="App text-center" >
-                <h1>Busca tú pokemon</h1>
+            <div className="App text-center" style={{ backgroundImage: "url(codigo.png)", backgroundSize: '100% 100%' }}>
+                <h1 class="text-light bg-dark">Busca tú pokemon</h1>
                 <input type="text" value={pokemon} onChange={onchangefn} />
-                <button type="" className="btn btn-light" onClick={onclickfn}>Buscar</button>
+                <button type="" className ="btn btn-light" onClick={onclickfn}>Buscar</button>
 <br></br>
                 <img src={pokemonD.sprites.front_default}></img>
                 <img src={pokemonD.sprites.back_default}></img>
 
-                <h2>{pokemonD.name}</h2>
+                <h2 className='text-light bg-dark'>{pokemonD.name}</h2>
             </div>
         </>
     );
